502e4d486e603dab6eb8c7ca9fd923bc21af9dcd
gtDan7 <kdanta13@gmail.com>
2023-04-28 13:51:13 +0700

    mengubah callback movies  menjadi fetch pada file index.js

--- index.js
+++ index.js
@@ -2,24 +2,10 @@ const send = document.querySelector('.send');
 const search = document.querySelector('.search');
 
 send.addEventListener('click',function(){
-    function getDataMovies(url,succes,error){
-      let xml = new XMLHttpRequest();
-
-      xml.onreadystatechange = function(){
-        if(xml.readyState === 4){
-          if(xml.status === 200){
-            succes(xml.response);
-          } else if(xml.status === 402){
-            error();
-          }
-        }
-      }
-      xml.open('get',url);
-      xml.send();
-    }
-    getDataMovies('http://www.omdbapi.com/?apikey=3a5b0446&s='+search.value,(hasil)=>{
-      let xlx = JSON.parse(hasil);
-      let movies = xlx.Search;
-      movies.forEach(m => console.info(m))
-    })
+    fetch('http://www.omdbapi.com/?apikey=3a5b0446&s='+ search.value)
+     .then(response => response.json())
+     .then(response =>{
+        let xlx = response.Search;
+        xlx.forEach(m => console.info(m))
+     })
 })
